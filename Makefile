5d349d24084fdd540da07c5ea8ff74e2916146d3
CODESOLE <canerselcuk4444@gmail.com>
2020-08-02 22:33:05 +0300

    implemented imgui

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 c:
-	clear;g++ -O3 main.cpp  Random.cpp Particle.cpp -o main -lsfml-graphics -lsfml-window -lsfml-system;./main;rm ./main
+	clear;g++ -O3 main.cpp imgui/imgui.cpp imgui/imgui_demo.cpp imgui/imgui_draw.cpp imgui/imgui_widgets.cpp imgui-sfml/imgui-SFML.cpp  Random.cpp Particle.cpp -o main -lGL -lsfml-graphics -lsfml-window -lsfml-system;./main;rm ./main
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "Particle.h"
 #include "Random.h"
+#include "imgui-sfml/imgui-SFML.h"
+#include "imgui/imgui.h"
+#include <SFML/Config.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Window/ContextSettings.hpp>
 #include <SFML/Window/Event.hpp>
@@ -10,6 +15,7 @@
 #include <SFML/Window/Mouse.hpp>
 #include <SFML/Window/Window.hpp>
 #include <SFML/Window/WindowStyle.hpp>
+#include <utility>
 
 int main()
 {
@@ -19,42 +25,53 @@ int main()
     std::srand(std::time(nullptr));
     sf::RenderWindow window(sf::VideoMode(1000, 500), "SFML works!", sf::Style::Default, ctx);
     window.setFramerateLimit(fps);
+    ImGui::SFML::Init(window);
     ParticleSystem p1{0.0f};
-    bool change = 0;
+    sf::Clock deltaClock;
+    float noise = 1.f, pLT = 1.f;
+    sf::Vector2f beginS{10.f, 10.f}, endS{20.f, 20.f};
+    int bc[3] = {255, 0, 0}, ec[3] = {0, 0, 255};
+    sf::Color beginC{(sf::Uint8)bc[0], (sf::Uint8)bc[1], (sf::Uint8)bc[2]},
+        endC{(sf::Uint8)ec[0], (sf::Uint8)ec[1], (sf::Uint8)ec[2]};
 
     while (window.isOpen())
     {
+        beginC.r = (sf::Uint8)bc[0];
+        beginC.g = (sf::Uint8)bc[1];
+        beginC.b = (sf::Uint8)bc[2];
+        endC.r = (sf::Uint8)ec[0];
+        endC.g = (sf::Uint8)ec[1];
+        endC.b = (sf::Uint8)ec[2];
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-                change = !change;
-            if (change)
-            {
-                p1.setNoisePower(3.f);
-                p1.setParticalLifeTime(0.5f);
-                p1.setBeginEndColor(sf::Color{0, 0, 255}, sf::Color{255, 0, 0});
-                p1.setBeginEndSize(sf::Vector2f{10.f, 10.f}, sf::Vector2f{20.f, 20.f});
-            }
-            else
-            {
-                p1.setNoisePower(2.f);
-                p1.setParticalLifeTime(1.f);
-                p1.setBeginEndColor(sf::Color{255, 0, 0}, sf::Color{0, 0, 255});
-                p1.setBeginEndSize(sf::Vector2f{20.f, 20.f}, sf::Vector2f{30.f, 30.f});
-            }
+            ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed ||
                 (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        p1.setNoisePower(std::move(noise));
+        p1.setParticalLifeTime(std::move(pLT));
+        p1.setBeginEndColor(std::move(beginC), std::move(endC));
+        p1.setBeginEndSize(sf::Vector2f{10.f, 10.f}, sf::Vector2f{20.f, 20.f});
 
         p1.OnParticleUpdate(sf::Mouse::getPosition(window), fps);
+
+        ImGui::Begin("Particle System Control Panel");
+        ImGui::SliderFloat("Noise Multiplier: ", &noise, 1.f, 5.f, "%.3f", 1.0f);
+        ImGui::SliderFloat("Particle Life Time: ", &pLT, 0.2f, 5.f, "%.3f", 0.1f);
+        ImGui::SliderInt3("Begin Color: ", &bc[0], 0, 255);
+        ImGui::SliderInt3("End Color: ", &ec[0], 0, 255);
+        ImGui::End();
 
         window.clear();
         for (auto &sprts : p1.getParticles())
             window.draw(sprts);
+        ImGui::SFML::Render(window);
         window.display();
     }
 
-    return 0;
+    ImGui::SFML::Shutdown();
 }
